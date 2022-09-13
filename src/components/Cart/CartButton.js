73befbc87740477d5c8ba1b69e58a2beeac6b6e9
SpencerTower <spencerjtower1@gmail.com>
2022-09-13 11:52:44 -0700

    installs redux toolkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,16 @@
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+    const toggleCartHandler = () => {
+      // if cart shown, hide cart and vice-versa
+    };
+
+    return (
+        <button className={classes.button} onClick={toggleCartHandler}>
+            <span>My Cart</span>
+            <span className={classes.badge}>1</span>
+        </button>
+    );
 };
 
 export default CartButton;
