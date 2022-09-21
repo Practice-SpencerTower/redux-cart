82a13994e51a66f8e72187c6d528b486a34a7eda
SpencerTower <spencerjtower1@gmail.com>
2022-09-21 15:48:42 -0700

    adds dummy items to Products, maps dummy items into ProductItem

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,18 +1,29 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
-const DUMMY_PRODUCTS = [];
+const DUMMY_ITEMS = [
+    {
+        id: 1,
+        title: 'Test',
+        price: 6,
+        description: 'This is a first product - amazing!',
+    },
+];
 
 const Products = (props) => {
     return (
         <section className={classes.products}>
             <h2>Buy your favorite products</h2>
             <ul>
-                <ProductItem
-                    title="Test"
-                    price={6}
-                    description="This is a first product - amazing!"
-                />
+                {DUMMY_ITEMS.map((item) => (
+                    <ProductItem
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        price={item.price}
+                        description={item.description}
+                    />
+                ))}
             </ul>
         </section>
     );
